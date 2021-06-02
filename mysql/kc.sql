115de3c3f80c4ad5e052c433c21f4b9f28403e8a
AmiSeverus <smlap@mail.ru>
2021-06-02 03:12:14 +0300

    mysql_database added

---
+++ mysql/kc.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 02 2021 г., 03:11
+-- Версия сервера: 5.7.29
+-- Версия PHP: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `kc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `api_users`
+--
+
+CREATE TABLE `api_users` (
+  `id` int(16) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `token` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `api_users`
+--
+
+INSERT INTO `api_users` (`id`, `username`, `password`, `token`) VALUES
+(1, '098f6bcd4621d373cade4e832627b4f6', '098f6bcd4621d373cade4e832627b4f6', NULL),
+(2, 'test', 'test', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `students`
+--
+
+INSERT INTO `students` (`id`, `name`) VALUES
+(1, 'Bernardo Santini'),
+(2, 'George Quebedo'),
+(3, 'Rob Shneider'),
+(4, 'Terry Cruz'),
+(5, 'David Smith'),
+(6, 'Tara Reed'),
+(7, 'Monika Sanchez'),
+(8, 'Richard Prize');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `api_users`
+--
+ALTER TABLE `api_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `api_users`
+--
+ALTER TABLE `api_users`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
